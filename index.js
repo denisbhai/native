2bef8237f802bf14028f70898750dfea88a7d625
denisnagani <denisnagani123@gmail.com>
2023-09-07 16:28:59 +0530

    ff

--- index.js
+++ index.js
@@ -8,6 +8,10 @@
 // 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
 //////////////////////////////////////////////////
 // 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
+//////////////////////////////////////////////////
+// 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
+//////////////////////////////////////////////////
+// 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
 
 import {AppRegistry} from 'react-native';
 import App from './src/index.js';
